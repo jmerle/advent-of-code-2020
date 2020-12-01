1e8b871f71e2c46bfce92207575bb72629a94377
Jasper van Merle <jaspervmerle@gmail.com>
2020-12-01 12:37:13 +0100

    Solve day 1

--- src/main/kotlin/com/jaspervanmerle/aoc2020/day/Day01.kt
+++ src/main/kotlin/com/jaspervanmerle/aoc2020/day/Day01.kt
@@ -1,11 +1,33 @@
 package com.jaspervanmerle.aoc2020.day
 
 class Day01 : Day(1) {
+    private val input = getInput()
+        .lines()
+        .map { it.toInt() }
+
     override fun solvePartOne(): Any {
-        TODO("Not yet implemented")
+        for (a in input) {
+            for (b in input) {
+                if (a + b == 2020) {
+                    return a * b
+                }
+            }
+        }
+
+        throw Error("Problem impossible")
     }
 
     override fun solvePartTwo(): Any {
-        TODO("Not yet implemented")
+        for (a in input) {
+            for (b in input) {
+                for (c in input) {
+                    if (a + b + c == 2020) {
+                        return a * b * c
+                    }
+                }
+            }
+        }
+
+        throw Error("Problem impossible")
     }
 }
