9db96e5d591ca32b3405a4de828712fad62af924
Jasper van Merle <jaspervmerle@gmail.com>
2020-12-06 14:06:21 +0100

    Improve solution to day 6

--- src/main/kotlin/com/jaspervanmerle/aoc2020/day/Day06.kt
+++ src/main/kotlin/com/jaspervanmerle/aoc2020/day/Day06.kt
@@ -3,17 +3,13 @@ package com.jaspervanmerle.aoc2020.day
 class Day06 : Day("6457", "3260") {
     private val groups = getInput()
         .split("\n\n")
-        .map {
-            it
-                .lines()
-                .map { line -> line.toCharArray().toList() }
-        }
+        .map { it.lines().map { line -> line.toCharArray().toSet() } }
 
     override fun solvePartOne(): Any {
         return groups.sumBy { it.flatten().toSet().size }
     }
 
     override fun solvePartTwo(): Any {
-        return groups.sumBy { it.fold(it[0].toSet()) { acc, list -> acc.intersect(list) }.size }
+        return groups.sumBy { it.fold(it[0]) { acc, list -> acc.intersect(list) }.size }
     }
 }
